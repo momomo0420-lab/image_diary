927a0123f9c629596951e241659772f6458487c5
momomo0420-lab <momomo0420.lab+github@gmail.com>
2023-09-28 20:41:45 +0900

    Tring out Firebase 1.

--- lib/app_container.dart
+++ lib/app_container.dart
@@ -1,5 +1,6 @@
 import 'package:image_diary/model/db/page_dao.dart';
 import 'package:image_diary/model/db/page_dao_impl.dart';
+import 'package:image_diary/model/db/page_dao_impl_with_cf.dart';
 import 'package:image_diary/model/page_repository.dart';
 import 'package:image_diary/model/page_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,7 +9,8 @@ part 'app_container.g.dart';
 
 @riverpod
 PageDao pageDao(PageDaoRef ref){
-  return PageDaoImpl();
+  // return PageDaoImpl();
+  return PageDaoImplWithCF();
 }
 
 @riverpod
---
+++ lib/model/db/page_dao_impl_with_cf.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_diary/model/db/page_dao.dart';
+
+class PageDaoImplWithCF extends PageDao {
+  static const collectionName = 'page_list';
+
+  @override
+  Future<void> deleteBy(int id) async {
+    // TODO: implement deleteBy
+    throw UnimplementedError();
+  }
+
+  /// ページリストの取得
+  ///
+  /// @return ページリスト
+  @override
+  Future<List<Map<String, dynamic>>> findAll() async {
+    List<Map<String, dynamic>> pageList = [];
+
+    // ページリストを取得
+    final db = FirebaseFirestore.instance;
+    final ref = await db.collection(collectionName).get();
+
+    // QueryDocumentSnapshot形式で取れているためMap形式へ変換
+    for(var page in ref.docs) {
+      pageList.add(page.data());
+    }
+
+    return pageList;
+  }
+
+  @override
+  Future<void> insert(Map<String, dynamic> page) async {
+    final db = FirebaseFirestore.instance;
+    final ref = await db.collection(collectionName).add(page);
+  }
+
+  @override
+  Future<void> update(Map<String, dynamic> page) async {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+}
